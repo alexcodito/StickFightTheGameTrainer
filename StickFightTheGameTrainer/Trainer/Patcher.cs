3bf8cb06c9c7b3e2f37bf21d36238c08fc2c051b
Alex <alex@local.local>
2020-08-03 21:01:35 +0100

    Add fix enabling bots to "have control"

--- StickFightTheGameTrainer/Trainer/Patcher.cs
+++ StickFightTheGameTrainer/Trainer/Patcher.cs
@@ -8,7 +8,6 @@ using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -362,6 +361,10 @@ namespace StickFightTheGameTrainer.Trainer
             await _logger.Log("Applying bot collision fix");
 
             await ApplyBotCollisionFix();
+            
+            await _logger.Log("Applying bot HasControl fix");
+
+            await ApplyBotHasControlFix();
 
             SaveAndReload(false);
 
@@ -399,9 +402,7 @@ namespace StickFightTheGameTrainer.Trainer
             var gameObjectTypeSig = unityEngineGameObjectTypeRefUser.ToTypeSig();
 
             var getGameObjectMethodSig = MethodSig.CreateInstance(gameObjectTypeSig);
-
-            var gameManagerStartMethodSig = MethodSig.CreateInstanceGeneric(0, new GenericMVar(0, gameManagerStartMethodDef));
-            gameManagerStartMethodSig.GenParamCount = 1;
+            var gameManagerStartMethodSig = MethodSig.CreateInstanceGeneric(1, new GenericMVar(0, gameManagerStartMethodDef));
 
             // {UnityEngine.GameObject UnityEngine.Component::get_gameObject()}
             var getGameObjectMethodRefUser = new MemberRefUser(_targetModule, new UTF8String("get_gameObject"), getGameObjectMethodSig, unityEngineComponentTypeRefUser);
@@ -895,6 +896,94 @@ namespace StickFightTheGameTrainer.Trainer
             else
             {
                 await _logger.Log("Apply bot collision fix: Controller set collision method signature does not match any instructions", LogLevel.Error);
+            }
+        }
+        
+        /// <summary>
+        /// Enable bots to be considered as in control and to go for guns. 
+        /// </summary>
+        /// <returns></returns>
+        private async Task ApplyBotHasControlFix()
+        {
+            // Fetch assembly refs
+            var unityEngine = _targetModule.GetAssemblyRef("UnityEngine");
+
+            // Fetch target type defs
+            var characterActionsTypeDef = _targetModule.Find("CharacterActions", true);
+            var controllerTypeDef = _targetModule.Find("Controller", true);
+            var aiTypeDef = _targetModule.Find("AI", true);
+
+            // Fetch target method defs
+            var aiStartMethodDef = aiTypeDef.FindMethod("Start");
+            var controllerStartMethodDef = controllerTypeDef.FindMethod("Start");
+            var characterActionsCreateWithControllerBindingsMethodDef = characterActionsTypeDef.FindMethod("CreateWithControllerBindings");
+
+            // Fetch target field defs
+            var aiGoForGunsFieldDef = aiTypeDef.FindField("goForGuns");
+            var controllerHasControlFieldDef = controllerTypeDef.FindField("mHasControl");
+            var controllerPlayerActionsFieldDef = controllerTypeDef.FindField("mPlayerActions");
+
+            // Fetch type ref users
+            var unityEngineComponentTypeRefUser = new TypeRefUser(_targetModule, "UnityEngine", "Component", unityEngine);
+            var unityEngineBehaviourTypeRefUser = new TypeRefUser(_targetModule, "UnityEngine", "Behaviour", unityEngine);
+
+            // Create method ref users
+            var getComponentMethodRefUser = new MemberRefUser(_targetModule, "GetComponent", MethodSig.CreateInstanceGeneric(1, new GenericMVar(0, aiStartMethodDef)), unityEngineComponentTypeRefUser);
+            //var getComponentMethodRefUser = new MemberRefUser(_targetModule, "GetComponent", MethodSig.CreateInstanceGeneric(1, aiTypeDef.ToTypeSig()), unityEngineComponentTypeRefUser);
+            //var getComponentMethodRefUser = new MemberRefUser(_targetModule, "GetComponent", MethodSig.CreateInstanceGeneric(1, new GenericVar(0, aiTypeDef)), unityEngineComponentTypeRefUser);
+            var setEnabledMethodRefUser = new MemberRefUser(_targetModule, "set_enabled", MethodSig.CreateInstance(_targetModule.CorLibTypes.Void, _targetModule.CorLibTypes.Boolean), unityEngineBehaviourTypeRefUser);
+
+            // Create method spec users
+            var getComponentMethodSpecUser = new MethodSpecUser(getComponentMethodRefUser, new GenericInstMethodSig(aiTypeDef.ToTypeSig()));
+
+            /*
+             
+                {IL_00C3: ldarg.0}
+                {IL_00C4: call AI UnityEngine.Component::GetComponent<AI>()}
+                {IL_00C9: ldc.i4.1}
+                {IL_00CA: callvirt System.Void UnityEngine.Behaviour::set_enabled(System.Boolean)}
+
+             */
+
+            var controllerStartInstructionSignature = new List<Instruction> {
+                new Instruction(OpCodes.Ldarg_0),
+                new Instruction(OpCodes.Call, getComponentMethodSpecUser),
+                new Instruction(OpCodes.Ldc_I4_1),
+                new Instruction(OpCodes.Callvirt, setEnabledMethodRefUser)
+            };
+
+            var matchedControllerStartMethodInstructions = InjectionHelpers.FetchInstructionsBySigComparerSignature(controllerStartMethodDef.Body.Instructions, controllerStartInstructionSignature);
+
+            if (matchedControllerStartMethodInstructions != null)
+            {
+                var lastMatchedInstruction = matchedControllerStartMethodInstructions.Last();
+                var injectionIndex = controllerStartMethodDef.Body.Instructions.IndexOf(lastMatchedInstruction) + 1;
+
+                var controllerStartInstructionsToInject = new List<Instruction>
+                {
+                    new Instruction(OpCodes.Ldarg_0),
+                    new Instruction(OpCodes.Call, getComponentMethodSpecUser),
+                    new Instruction(OpCodes.Ldc_I4_1),
+                    new Instruction(OpCodes.Stfld, aiGoForGunsFieldDef),
+                    new Instruction(OpCodes.Ldarg_0),
+                    new Instruction(OpCodes.Ldc_I4_1),
+                    new Instruction(OpCodes.Stfld, controllerHasControlFieldDef),
+                    new Instruction(OpCodes.Ldarg_0),
+                    new Instruction(OpCodes.Call, characterActionsCreateWithControllerBindingsMethodDef),
+                    new Instruction(OpCodes.Stfld, controllerPlayerActionsFieldDef),
+                };
+
+                // Add new instructions after the matched signature
+                for (var i = 0; i < controllerStartInstructionsToInject.Count; i++)
+                {
+                    controllerStartMethodDef.Body.Instructions.Insert(injectionIndex + i, controllerStartInstructionsToInject[i]);
+                }
+
+                controllerStartMethodDef.Body.UpdateInstructionOffsets();
+            }
+            else
+            {
+                await _logger.Log("Apply bot HasControl fix: Controller start method signature does not match any instructions", LogLevel.Error);
             }
         }
 
